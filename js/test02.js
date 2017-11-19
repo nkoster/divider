1478a24e6c6dc6b2e684daa47751ac915caeab39
Niels Koster <n.koster@portavita.eu>
2017-11-19 02:31:15 +0100

    Hor slider div

--- js/test02.js
+++ js/test02.js
@@ -5,18 +5,22 @@
         counter = 1;
 
     function newElement(background) {
-        
+
         var
             dWest = document.createElement("div"),
             dEast = document.createElement("div"),
+            dHor = document.createElement("div"),
             dWestId = 'd' + counter + '-w',
             dEastId = 'd' + counter + '-e',
+            dHorId = 'd' + counter,
             dParentId = 'd' + (counter - 1) + '-e',
             dParent = document.getElementById(dParentId);
 
         dWest.setAttribute('id', 'd' + counter + '-w');
         dEast.setAttribute('id', 'd' + counter + '-e');
+        dHor.setAttribute('id', 'd' + counter);
         dParent.appendChild(dWest);
+        dParent.appendChild(dHor);
         dParent.appendChild(dEast);
 
         var
@@ -39,6 +43,15 @@
         document.getElementById(dEastId).style.width = width + (width === 0 ? '' : 'px');
         document.getElementById(dEastId).style.height = '100vh';
         document.getElementById(dEastId).style.background = 'green';
+
+        document.getElementById(dHorId).style.position = 'absolute';
+        document.getElementById(dHorId).style.left = width + (width === 0 ? '' : 'px');
+        document.getElementById(dHorId).style.top = '0';
+        document.getElementById(dHorId).style.width = '8px';
+        document.getElementById(dHorId).style.height = '100vh';
+        document.getElementById(dHorId).style.background = 'black';
+        document.getElementById(dHorId).style.cursor = 'ew-resize';
+        document.getElementById(dHorId).style.zIndex = '999';
 
         counter += 1;
     }
