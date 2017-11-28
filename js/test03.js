1d84032ea478c8c28f2e2959f2eb3fc662c96c3e
Niels Koster <n.koster@portavita.eu>
2017-11-28 10:11:42 +0100

    try catch

--- js/test03.js
+++ js/test03.js
@@ -18,30 +18,29 @@
         e = e || window.event;
         var
             elementId = (e.target || e.srcElement).id,
-            eId = parseInt(elementId.substr(1));
+            eId = parseInt(elementId.substr(1)),
+            realId = elementId;
         if (eId % 2 === 0) {
             eId++;
             elementId = 'd' + eId;
         }
-        var
-            drag = document.getElementById(elementId),
-            next = document.getElementById(document.getElementById(elementId).nextElementSibling.id),
-            slideLimitRight = parseInt(document.getElementById(next.nextElementSibling.id).style.left),
-            slideLimitLeft;
-        if (document.getElementById(document.getElementById(elementId).previousElementSibling)) {
+        try {
             var
-                prev = document.getElementById(document.getElementById(elementId).previousElementSibling.id);
-            slideLimitLeft = parseInt(prev.style.left);
-        } else {
-            slideLimitLeft = 0;
+                drag = document.getElementById(elementId),
+                prev = document.getElementById(document.getElementById(elementId).previousElementSibling.id),
+                next = document.getElementById(document.getElementById(elementId).nextElementSibling.id),
+                slideLimitRight = parseInt(document.getElementById(next.nextElementSibling.id).style.left),
+                slideLimitLeft = parseInt(prev.style.left);
+            if (!slideLimitRight) slideLimitRight = screen.width;
+            if (isNaN(slideLimitLeft)) slideLimitLeft = 0;
+            if ((e.clientX > (slideLimitLeft + 20)) && (e.clientX < (slideLimitRight - 20))) {
+                drag.style.left = e.clientX + (e.clientX === 0 ? '' : 'px');
+                next.style.left = e.clientX + (e.clientX === 0 ? '' : 'px');
+            }
+            document.getElementById('debug').innerText = elementId;
+        } catch (e) {
+            document.getElementById('debug').innerText = 'error: ' + realId;
         }
-        if (!slideLimitRight) slideLimitRight = screen.width;
-        if (isNaN(slideLimitLeft)) slideLimitLeft = 0;
-        if ((e.clientX > (slideLimitLeft + 20)) && (e.clientX < (slideLimitRight - 20))) {
-            drag.style.left = e.clientX + (e.clientX === 0 ? '' : 'px');
-            next.style.left = e.clientX + (e.clientX === 0 ? '' : 'px');
-        }
-        document.getElementById('debug').innerText = elementId;
     }
 
     document.getElementById('d1').addEventListener('mousedown', mouseDown, true);
